aa581b5ee3a4ac32fee0618293c6970267abceab
Andrew A. Viktorov <nasa51@berdyansk.net>
2009-05-25 20:42:53 +0300

    fix routing to work with sf1.2

--- lib/sfGallery2Routing.class.php
+++ lib/sfGallery2Routing.class.php
@@ -12,6 +12,15 @@ class sfGallery2Routing
     $r = $event->getSubject();
     $embed_uri = sfConfig::get('app_gallery2_embed_uri');
     // Preprend our route
-    $r->prependRoute('sf_gallery2_plugin', $embed_uri.'/:action', array('module' => 'sfGallery2Plugin', 'action' => 'index'));
+    // prepend our routes
+    if (class_exists('sfRoute')) // Symfony 1.1 compatibility. Checks for sfRoute because it is not implemented in 1.1
+    {
+      $r->prependRoute('sf_gallery2_plugin', new sfRoute($embed_uri.'/:action', array('module' => 'sfGallery2Plugin', 'action' => 'index')));
+    }
+    else
+    {
+      $r->prependRoute('sf_gallery2_plugin', $embed_uri.'/:action', array('module' => 'sfGallery2Plugin', 'action' => 'index'));
+    }
+
   }
 }
